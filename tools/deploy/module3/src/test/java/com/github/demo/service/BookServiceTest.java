54e6784584779e8f18ed0ef8761b9ae89507b872
viviana222601 <86531853+viviana222601@users.noreply.github.com>
2025-04-09 16:51:16 -0500

    Update BookServiceTest.java

--- tools/deploy/module3/src/test/java/com/github/demo/service/BookServiceTest.java
+++ tools/deploy/module3/src/test/java/com/github/demo/service/BookServiceTest.java
@@ -15,7 +15,7 @@ import static org.junit.Assert.assertEquals;
 public class BookServiceTest {
 
     // Testing API token key
-    private static final String API_TOKEN = "AIzaSyAQfxPJiounkhOjODEO5ZieffeBv6yft2Q";
+    private static final String API_TOKEN = "AIzaSyAQfxPJiounkhOjODEO5ZieffeBv6yft2K";//Q
     
     private BookService bookService;
 
